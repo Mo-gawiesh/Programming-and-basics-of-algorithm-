2b3dd9fc4331f9662c0c57d19974b51085dfbbd9
Mogawish <119753320+Mogawish@users.noreply.github.com>
2023-10-07 22:21:28 +0300

    fix

--- Laba4/myfuncs.cpp
+++ Laba4/myfuncs.cpp
@@ -4,22 +4,39 @@
 using namespace std;
 
 int one_or_zero(int a) {
-    int m;
-    while (!(cin >> m) || (a = m) != m || (m != 0 && m != 1)) {
-        cout << "!!!Error!!!" << endl;
-        cout << "Input 1 or 0" << endl;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    string input;
+    while (true) {
+        cin >> input;
+        if (input != "0" && input != "1") {
+            cout << "!!!Error!!!" << endl;
+            cout << "Input 1 or 0" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        } else {
+            a = stoi(input); // Convert the valid input to an integer
+            break;           // Exit the loop when valid input is received
+        }
     }
     return a;
 }
 
+
+
+
+
+
+
 int natural(int n) {
     int m;
-    while (!(cin >> m) || m <= 0 || (n = m) != m) {
-        cout << "Error, try again:" << endl;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    while (true) {
+        if (!(cin >> m) || m <= 0) {
+            cout << "Error, enter a positive integer:" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        } else {
+            n = m; // Assign the valid input value to 'n'
+            break;  // Exit the loop when valid input is received
+        }
     }
     return n;
 }
--- Laba4/myfuncs.h
+++ Laba4/myfuncs.h
@@ -1,5 +1,4 @@
 #pragma once
-
 int one_or_zero(int a);
 int natural(int n);
 char symbol(char c);
