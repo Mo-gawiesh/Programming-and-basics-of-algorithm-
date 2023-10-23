d6230ffe785179456287a31e410938ac37902e5b
Mogawish <119753320+Mogawish@users.noreply.github.com>
2023-10-23 14:11:34 +0300

    1

--- Laba1/Laba1/main.cpp
+++ Laba1/Laba1/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <memory>
 #include "myfuncs.h"
 #include "constants.h"
+#include <cstdlib> // Include the <cstdlib> header for the exit function
 
 int main() {
     int n{0};
@@ -18,13 +20,13 @@ int main() {
 
         if (k == 0) {
             n = getNatural("Input n: "); // Get a valid positive integer
-            double* a = new double[n];
+            std::unique_ptr<double[]> a = std::make_unique<double[]>(n);
 
             for (int i = 0; i < n; i++) {
                 a[i] = getRealNumber("Input a" + std::to_string(i + 1) + ": "); // Get a valid real number
             }
 
-            double* b = func(a, n);
+            auto b = func(a.get(), n); // Capture the result in auto
 
             if (l == 1) {
                 std::ofstream file(OUTPUT_FILE_PATH);
@@ -41,38 +43,39 @@ int main() {
                 std::cout << "Result:\n";
                 for (int i = 0; i < n; i++) std::cout << "b" << i + 1 << "=" << b[i] << std::endl;
             }
-
-            delete[] a;
-            delete[] b;
         } else if (k == 1) {
             std::ifstream file(INPUT_FILE_PATH);
-            file >> n;
-            double* a = new double[n];
+            if (file.is_open()) {
+                file >> n;
+                std::unique_ptr<double[]> a = std::make_unique<double[]>(n);
 
-            for (int i = 0; i < n; i++) {
-                file >> a[i];
+                for (int i = 0; i < n; i++) {
+                    file >> a[i];
+                }
+
+                auto b = func(a.get(), n); // Capture the result in auto
+
+                if (l == 1) {
+                    std::ofstream outFile(OUTPUT_FILE_PATH);
+                    outFile << "Input data:\n";
+                    outFile << "n=" << n << std::endl;
+                    for (int i = 0; i < n; i++) outFile << "a" << i + 1 << "=" << a[i] << std::endl;
+                    outFile << "Result:\n";
+                    for (int i = 0; i < n; i++) outFile << "b" << i + 1 << "=" << b[i] << std::endl;
+                    outFile.close();
+                } else if (l == 0) {
+                    std::cout << "Input data:\n";
+                    std::cout << "n=" << n << std::endl;
+                    for (int i = 0; i < n; i++) std::cout << "a" << i + 1 << "=" << a[i] << std::endl;
+                    std::cout << "Result:\n";
+                    for (int i = 0; i < n; i++) std::cout << "b" << i + 1 << "=" << b[i] << std::endl;
+                }
+
+                file.close();
+                exit(0); // Terminate the program after processing the file data
+            } else {
+                std::cout << "Error: Unable to open the input file." << std::endl;
             }
-
-            double* b = func(a, n);
-
-            if (l == 1) {
-                std::ofstream outFile(OUTPUT_FILE_PATH);
-                outFile << "Input data:\n";
-                outFile << "n=" << n << std::endl;
-                for (int i = 0; i < n; i++) outFile << "a" << i + 1 << "=" << a[i] << std::endl;
-                outFile << "Result:\n";
-                for (int i = 0; i < n; i++) outFile << "b" << i + 1 << "=" << b[i] << std::endl;
-                outFile.close();
-            } else if (l == 0) {
-                std::cout << "Input data:\n";
-                std::cout << "n=" << n << std::endl;
-                for (int i = 0; i < n; i++) std::cout << "a" << i + 1 << "=" << a[i] << std::endl;
-                std::cout << "Result:\n";
-                for (int i = 0; i < n; i++) std::cout << "b" << i + 1 << "=" << b[i] << std::endl;
-            }
-
-            delete[] a;
-            delete[] b;
         }
 
         std::cout << "Continue (0-no; 1-yes)? ";
